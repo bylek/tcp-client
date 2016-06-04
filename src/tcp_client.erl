367bf30314f82f4b4fdc012e45992da729a7eb35
Paweł Kapała <bylek77@gmail.com>
2016-06-04 08:21:10 +0200

    timers

--- src/tcp_client.erl
+++ src/tcp_client.erl
@@ -4,7 +4,6 @@
 %%% @doc
 %%%
 %%% @end
-%%% Created : 02. Jun 2016 19:01
 %%%-------------------------------------------------------------------
 -module(tcp_client).
 -author("bylek").
@@ -28,7 +27,18 @@ connect(0) -> ok.
 
 send() ->
   {ok, Socket} = gen_tcp:connect("localhost", ?Port, [{active, false}, {packet, 2}]),
+
+  statistics(runtime),
+  statistics(wall_clock),
+
   gen_tcp:send(Socket, "Some Data"),
   {ok, Msg} = gen_tcp:recv(Socket, 0),
+
+  {_, Time1} = statistics(runtime),
+  {_, Time2} = statistics(wall_clock),
+  U1 = Time1 * 1000,
+  U2 = Time2 * 1000,
+  io:format("Code time=~p (~p) microseconds~n", [U1,U2]),
+
   gen_tcp:close(Socket),
   Msg.
